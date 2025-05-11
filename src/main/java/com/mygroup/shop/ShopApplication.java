8639eaa647beeed0ec81cb24b43028eb1fdc520c
a-maystorov <alkin971@gmail.com>
2025-05-11 14:11:14 +0200

    define one to one relation between user and profile

--- src/main/java/com/mygroup/shop/ShopApplication.java
+++ src/main/java/com/mygroup/shop/ShopApplication.java
@@ -1,6 +1,6 @@
 package com.mygroup.shop;
 
-import com.mygroup.shop.entities.Tag;
+import com.mygroup.shop.entities.Profile;
 import com.mygroup.shop.entities.User;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,16 +14,11 @@ public class ShopApplication {
                 .password("password")
                 .build();
 
-        var tag1 = new Tag("tag1");
-        var tag2 = new Tag("tag2");
+        var profile = Profile.builder().bio("bio").build();
 
-        user.addTag(tag1);
-        user.addTag(tag2);
+        user.setProfile(profile);
+        profile.setUser(user);
 
-        System.out.println("Before removal: " + user);
-
-        user.removeTag(tag1);
-
-        System.out.println("After removal: " + user);
+        System.out.println(user);
     }
 }
--- src/main/java/com/mygroup/shop/entities/Profile.java
+++ src/main/java/com/mygroup/shop/entities/Profile.java
@@ -1,11 +1,14 @@
 package com.mygroup.shop.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Getter
 @Setter
 @Entity
@@ -27,4 +30,10 @@ public class Profile {
 
     @Column(name = "loyalty_points")
     private Integer loyaltyPoints;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    @MapsId
+    @ToString.Exclude
+    private User user;
 }
