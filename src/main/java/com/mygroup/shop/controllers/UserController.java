cc260af0d71447093b55a2cf233e7a744f32dc57
a-maystorov <alkin971@gmail.com>
2025-05-13 12:55:02 +0200

    handle validation errors with jakarta

--- src/main/java/com/mygroup/shop/controllers/UserController.java
+++ src/main/java/com/mygroup/shop/controllers/UserController.java
@@ -6,13 +6,17 @@ import com.mygroup.shop.dtos.UpdateUserRequest;
 import com.mygroup.shop.dtos.UserDto;
 import com.mygroup.shop.mappers.UserMapper;
 import com.mygroup.shop.repositories.UserRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -47,7 +51,7 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserDto> createUser(
-            @RequestBody RegisterUserRequest request,
+            @Valid @RequestBody RegisterUserRequest request,
             UriComponentsBuilder uriBuilder
     ) {
         var user = userMapper.toEntity(request);
@@ -99,10 +103,21 @@ public class UserController {
         if (!user.getPassword().equals(request.getOldPassword())) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
-        
+
         user.setPassword(request.getNewPassword());
         userRepository.save(user);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException exception) {
+        var errors = new HashMap<String, String>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        
+        return ResponseEntity.badRequest().body(errors);
     }
 }
