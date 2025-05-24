0fffbc9a7b6e6880d5c0dfd7cd6bfa5e482317e2
a-maystorov <alkin971@gmail.com>
2025-05-24 12:46:32 +0200

    handle stripe errors

--- src/main/java/com/mygroup/shop/controllers/CheckoutController.java
+++ src/main/java/com/mygroup/shop/controllers/CheckoutController.java
@@ -1,13 +1,14 @@
 package com.mygroup.shop.controllers;
 
 import com.mygroup.shop.dtos.CheckoutRequest;
-import com.mygroup.shop.dtos.CheckoutResponse;
 import com.mygroup.shop.dtos.ErrorDto;
 import com.mygroup.shop.exceptions.CartEmptyException;
 import com.mygroup.shop.exceptions.CartNotFoundException;
 import com.mygroup.shop.services.CheckoutService;
+import com.stripe.exception.StripeException;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,8 +19,14 @@ public class CheckoutController {
     private final CheckoutService checkoutService;
 
     @PostMapping
-    public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request) {
-        return checkoutService.checkout(request);
+    public ResponseEntity<?> checkout(@Valid @RequestBody CheckoutRequest request) {
+        try {
+            return ResponseEntity.ok(checkoutService.checkout(request));
+        } catch (StripeException ex) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorDto("Error creating a checkout session"));
+        }
     }
 
     @ExceptionHandler({CartNotFoundException.class, CartEmptyException.class})
