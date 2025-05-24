25f6ff50f158ed3405a7697cfb826bf0829e8a26
a-maystorov <alkin971@gmail.com>
2025-05-24 13:10:24 +0200

    add webhook endpoint

--- src/main/java/com/mygroup/shop/controllers/CheckoutController.java
+++ src/main/java/com/mygroup/shop/controllers/CheckoutController.java
@@ -7,21 +7,54 @@ import com.mygroup.shop.exceptions.CartEmptyException;
 import com.mygroup.shop.exceptions.CartNotFoundException;
 import com.mygroup.shop.exceptions.PaymentException;
 import com.mygroup.shop.services.CheckoutService;
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.net.Webhook;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/checkout")
 public class CheckoutController {
     private final CheckoutService checkoutService;
 
+    @Value("${stripe.webhookSecretKey}")
+    private String webhookSecretKey;
+
     @PostMapping
     public CheckoutResponse checkout(@Valid @RequestBody CheckoutRequest request) {
         return checkoutService.checkout(request);
+    }
+
+    @PostMapping("/webhook")
+    public ResponseEntity<Void> handleWebhook(
+            @RequestHeader("Stripe-Signature") String signature,
+            @RequestBody String payload
+    ) {
+        try {
+            var event = Webhook.constructEvent(payload, signature, webhookSecretKey);
+            System.out.println(event.getType());
+
+            var stripeObject = event.getDataObjectDeserializer().getObject().orElse(null);
+
+            switch (event.getType()) {
+                case "payment_intent.succeeded" -> {
+                    // Update order status (PAID)
+                }
+                case "payment_intent.failed" -> {
+                    // Update order status (FAILED)
+                }
+            }
+
+            return ResponseEntity.ok().build();
+
+        } catch (SignatureVerificationException e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @ExceptionHandler(PaymentException.class)
@@ -30,8 +63,7 @@ public class CheckoutController {
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorDto("Error creating a checkout session"));
     }
-
-
+    
     @ExceptionHandler({CartNotFoundException.class, CartEmptyException.class})
     public ResponseEntity<ErrorDto> handleException(Exception ex) {
         return ResponseEntity.badRequest().body(new ErrorDto(ex.getMessage()));
